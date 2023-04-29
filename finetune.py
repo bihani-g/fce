597d581128b4c2dd2d7ca0a0ab62814e8a64276e
bihani-g <gbihani@purdue.edu>
2023-04-29 01:00:50 -0400

    demo data added

--- finetune.py
+++ finetune.py
@@ -1,38 +1,46 @@
-import datasets
-import csv
 import torch
 from datasets import load_dataset
 from pytorch_lightning import Trainer, seed_everything
 from utils import *
 import argparse
-import csv
 import warnings
-import gc
-from os.path import exists
 import pickle
-
+import numpy as np
+import os
+import torch
+import pandas as pd
+import skfuzzy
+from tqdm import tqdm
+from calibration_utils import *
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
-##----add argparser
-# parser = argparse.ArgumentParser(description='Data setup')
-# parser.add_argument('ds_name', type = str,
-#                     help='name of the dataset (news/agnews/imdb)')
-# parser.add_argument('size', type=int,
-#                     help='number of entries in training data (50/100/500/1000/2000/5000/10000)')
-
-# args = parser.parse_args()
-
-# size = args.size
-# ds_name = args.ds_name
-# data_dir = ds_name+str(size)+".hf"
-
-# size = 50
-# ds_name = 'news'
-# data_dir = ds_name+str(size)+".hf"
+#-------------------------------------------------------------------------------------------------------------------#
+parser = argparse.ArgumentParser(description='Data setup')
+parser.add_argument('ds_name', type=str,
+                    help='Dataset name (news/agnews/imdb)')
+parser.add_argument('size', type=int,
+                    help='Train data size')
+parser.add_argument('bins', type=int,
+                    help='Number of bins to calculate calibration error')
+parser.add_argument('data_dir', type=str,
+                    help='Directory location to save data')
+parser.add_argument('result_dir', type=str,
+                    help='Directory location to save results')
 
 
-# load hf dataset
+args = parser.parse_args()
+
+size = args.size
+ds_name = args.ds_name
+filename = ds_name + str(size) + ".hf"
+data_dir = args.data_dir
+result_dir = args.result_dir
+n_bins = args.bins
+
+#-------------------------------------------------------------------------------------------------------------------#
+# load dataset
 if ds_name == 'news':
     data = load_dataset("SetFit/20_newsgroups")
 elif ds_name == 'agnews':
@@ -40,77 +48,143 @@ elif ds_name == 'agnews':
 elif ds_name == 'imdb':
     data = load_dataset("imdb")
 
-
 # saving dataset variation
-create_data_split(data, size, ds_name)
-    
-#setup input data
+create_data_split(data, size, ds_name, data_dir)
+
+# setup input data
 seed_everything(42)
 
-dm = finetuning_data(model_name_or_path="bert-base-cased", data_name = ds_name, data_dir=data_dir, data_path = "./conf_data", )
+dm = finetuning_data(model_name_or_path="bert-base-cased", data_name=ds_name, filename = filename,
+                     data_dir=data_dir)
 dm.setup("fit")
 
 print("Train: {}".format(len(dm.dataset['train'])))
 print("Test: {}".format(len(dm.dataset['test'])))
 
+#-------------------------------------------------------------------------------------------------------------------#
+# define model parameters
+model = finetuner(model_name_or_path="bert-base-cased", num_labels=dm.num_labels)
 
-#define model parameters
-model = finetuner(model_name_or_path="bert-base-cased", num_labels = dm.num_labels)
-
-#define training hyperparameters
+# define training hyperparameters
 trainer = Trainer(
     max_epochs=1,
-    accelerator = "auto",
+    accelerator="auto",
     devices=1 if torch.cuda.is_available() else None)
 
-#model training
+# model training
 trainer.fit(model, datamodule=dm)
-print("Training done!")
-
-#model eval
-metrs = trainer.test(model, datamodule = dm)
-print("Testing done!")
-outputs = trainer.predict(model, datamodule = dm, return_predictions=True)
-
-print(metrs)
-
 print("Fine-tuning done!")
 
+# model eval
+metrs = trainer.test(model, datamodule=dm)
+outputs = trainer.predict(model, datamodule=dm, return_predictions=True)
+
+print(metrs)
 
 soft_preds = [y for x in outputs for y in x[0]]
 preds = [y for x in outputs for y in x[1]]
 labels = [y for x in outputs for y in x[2]]
 
-
-
-dir_path = "./demo_results"
+dir_path = result_dir
 # Check whether the specified path exists or not
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
-print(" saving confidence data")
-filename = ds_name+str(size)
-preds_file = 'preds'+filename+'.pickle'
-soft_preds_file = 'soft_preds'+filename+'.pickle'
-labels_file = 'labels'+filename+'.pickle'
+print("Saving prediction probabilities...")
+
+filename = ds_name + str(size)
+preds_file = 'predicted_labels' + filename + '.pickle'
+soft_preds_file = 'predict_probs' + filename + '.pickle'
+labels_file = 'labels' + filename + '.pickle'
 
 preds_path = os.path.join(dir_path, preds_file)
 soft_preds_path = os.path.join(dir_path, soft_preds_file)
 labels_path = os.path.join(dir_path, labels_file)
 
-
 # save
 with open(soft_preds_path, 'wb') as handle:
     pickle.dump(soft_preds, handle)
 
-print("soft preds saved!")
-
 with open(preds_path, 'wb') as handle:
     pickle.dump(preds, handle)
-
-print("preds saved!")
 
 with open(labels_path, 'wb') as handle:
     pickle.dump(labels, handle)
     
-print("labels saved!")
+print("Saved prediction probabilities!")
+#-------------------------------------------------------------------------------------------------------------------#
+# analysis of predictions
+
+data_name = ds_name+str(size)
+
+ece_list = []
+fce_list = []
+uf_list = []
+of_list = []
+bins_list = []
+ece_breakdown = []
+fce_breakdown = []
+
+print("Binning probabilities...")
+
+for n_bins in tqdm(range(1, n_bins+1)):
+    with open(os.path.join(result_dir,'predict_probs'+data_name+'.pickle'), 'rb') as handle:
+        soft_preds = pickle.load(handle)
+    with open(os.path.join(result_dir,'predicted_labels'+data_name+'.pickle'), 'rb') as handle:
+        preds = pickle.load(handle)
+    with open(os.path.join(result_dir,'labels'+data_name+'.pickle'), 'rb') as handle:
+        labels = pickle.load(handle)
+
+    incorrect = []
+    correct = []
+    for i in range(len(preds)):
+        if preds[i] != labels[i]:
+            incorrect.append([torch.softmax(soft_preds[i],0), int(preds[i]), int(labels[i])])
+        else:
+            correct.append([torch.softmax(soft_preds[i],0), int(preds[i]), int(labels[i])])
+
+    # calculate overconfidence --> expectation of confidence over incorrect predictions
+    of = []
+    for i in range(len(incorrect)):
+        of.append(max(incorrect[i][0]))
+
+    # calculate underconfidence --> expectation of 1-confidence over correct predictions
+    uf = []
+    for i in range(len(correct)):
+        uf.append(1-max(correct[i][0]))
+
+
+    uf_list.append(np.mean(uf))
+    of_list.append(np.mean(of))
+
+    ece_dict = {'soft_preds': np.array([np.array(torch.softmax(x,0)) for x in soft_preds]), 
+        'preds': [int(x) for x in preds], 
+        'labels': [int(x) for x in labels]}
+
+
+    ece_vals, ece = expected_calibration_error(np.array(ece_dict['labels']), 
+                                                        ece_dict['soft_preds'], num_bins=n_bins)
+
+    bins_list.append(n_bins)
+    ece_list.append(ece)
+    ece_breakdown.append(ece_vals)
+    print("Done!")
+    
+    fce_vals, fce = fuzzy_calibration_error(np.array(ece_dict['labels']), ece_dict['soft_preds'], n_bins)
+
+    fce_list.append(fce)
+    fce_breakdown.append(fce_vals)
+    print("Done!")
+
+
+
+print("Saving calibration evaluation results...")
+data = pd.DataFrame(list(zip(bins_list, uf_list, of_list, 
+                             ece_list, ece_breakdown, fce_list, fce_breakdown)), 
+               columns =['bins', 'uf', 'of', 'ece', 'ece_breakdown',
+                         'fce', 'fce_breakdown'])
+
+data.to_csv(os.path.join(result_dir,'calibration_eval.csv'), index = False)
+print("Done!")
+
+
--- utils.py
+++ utils.py
@@ -1,38 +1,24 @@
+import os
+import datasets
+import evaluate
 import numpy as np
 import pandas as pd
-import datasets
 import torch
-from datasets import load_from_disk, DatasetDict, Dataset
-from torch.utils.data import DataLoader
-import math
-from tqdm import tqdm
-import evaluate
-from typing import Text, Iterable, Optional
-
-import matplotlib.pyplot as plt
-from sklearn import metrics
-
-import tensorflow as tf
+from datasets import DatasetDict, Dataset
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
-from typing import Optional
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 from transformers import (
     AdamW,
     AutoConfig,
-    AutoModel,
     AutoModelForSequenceClassification,
     AutoTokenizer,
     get_linear_schedule_with_warmup,
 )
-from random import sample
-from utils import *
-from datasets import concatenate_datasets, interleave_datasets
-import nltk
-nltk.download('wordnet'); nltk.download('averaged_perceptron_tagger'); nltk.download('stopwords')
-
-from gensim.test.utils import common_texts
+from typing import Optional
 from utils import *
 tqdm.pandas()
-import os
+
 
 ####----datasetup utils----####
 
@@ -41,54 +27,39 @@ def preprocess(text):
         text = pd.NA
     return text
 
-    
-def strat_sampler(split, n):  
-    labels = list(set(split['label']))
 
+def strat_sampler(split, n):
     # stratified sampling on preprocessed train data
     strat_data = split.groupby('label', group_keys=False).apply(lambda x:
-                                                            x.sample(int(np.rint(n*len(x)/len(split))))).sample(frac=1).reset_index(drop=True)
-    
+                                                                x.sample(int(np.rint(n * len(x) / len(split))))).sample(
+        frac=1).reset_index(drop=True)
+
     return strat_data
+
 
 def drop_empty(data_split):
     split = pd.DataFrame(data_split)
     split["text"] = split["text"].apply(preprocess)
     split = split[split["text"].notna()]
-     
+
     return split
-    
-    
-    
-def create_data_split(data, size, ds_name):    
-    data_n = DatasetDict({"train":Dataset.from_pandas(strat_sampler(drop_empty(data['train']), 
-                                                                    size)),
-                          "test":Dataset.from_pandas(drop_empty(data['test']))})
-    data_path = "./conf_data"
+
+
+def create_data_split(data, size, ds_name, data_dir):
+    data_n = DatasetDict({"train": Dataset.from_pandas(strat_sampler(drop_empty(data['train']),
+                                                                     size)),
+                          "test": Dataset.from_pandas(drop_empty(data['test']))})
+    data_dir = data_dir
     # Check whether the specified path exists or not
-    if not os.path.exists(data_path):
-        os.makedirs(data_path)
-        
-    data_n.save_to_disk(data_path+"/"+ds_name+str(size)+".hf")
-    
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
 
-    
+    data_n.save_to_disk(data_dir + "/" + ds_name + str(size) + ".hf")
+
+
 ####----training utils----####
-   
-def print_gpu_utilization():
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU memory occupied: {info.used//1024**2} MB.")
 
-
-def print_summary(result):
-    print(f"Time: {result.metrics['train_runtime']:.2f}")
-    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
-    
-    
 class finetuning_data(LightningDataModule):
-
     task_field_map = {
         "ft": ["text"],
         "mia": ["predictions"]}
@@ -108,20 +79,20 @@ class finetuning_data(LightningDataModule):
     ]
 
     def __init__(
-        self,
-        model_name_or_path: str,
-        data_path: str = "./conf_data",
-        data_dir: str = "news1000.hf",
-        data_name: str = "news",
-        max_seq_length: int = 256,
-        train_batch_size: int = 4,
-        eval_batch_size: int = 4,
-        **kwargs,
-    ):  
+            self,
+            model_name_or_path: str,
+            data_dir: str = "./data",
+            filename: str = "news1000.hf",
+            data_name: str = "news",
+            max_seq_length: int = 256,
+            train_batch_size: int = 4,
+            eval_batch_size: int = 4,
+            **kwargs,
+    ):
         super().__init__()
         self.model_name_or_path = model_name_or_path
-        self.data_path = data_path
         self.data_dir = data_dir
+        self.filename = filename
         self.data_name = data_name
         self.train_batch_size = train_batch_size
         self.eval_batch_size = eval_batch_size
@@ -130,28 +101,22 @@ class finetuning_data(LightningDataModule):
         self.num_labels = self.task_num_labels[self.data_name]
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
 
-         
-        
-
-
     def setup(self, stage: str):
-        self.original_dataset = datasets.load_from_disk(os.path.join(self.data_path, self.data_dir))
+        self.original_dataset = datasets.load_from_disk(os.path.join(self.data_dir, self.filename))
         test_dataset = pd.DataFrame(self.original_dataset['test'])
         train_dataset = pd.DataFrame(self.original_dataset['train'])
 
-            #randomly shuffle rows
-            sampled = train_dataset.sample(frac = 1).reset_index(drop=True)
-        
-        combined_dataset = pd.concat([train_dataset,sampled], ignore_index=True)
-        combined_dataset = combined_dataset.sample(frac = 1).reset_index(drop=True) # shuffle original and augmented rows
+        # randomly shuffle rows
+        sampled = train_dataset.sample(frac=1).reset_index(drop=True)
+
+        combined_dataset = pd.concat([train_dataset, sampled], ignore_index=True)
+        combined_dataset = combined_dataset.sample(frac=1).reset_index(drop=True)  # shuffle original and augmented rows
         combined_dataset["label"] = pd.to_numeric(combined_dataset["label"])
 
-
         test_dataset = pd.DataFrame(self.original_dataset['test'])
-        
-        self.dataset = DatasetDict({"train":Dataset.from_pandas(combined_dataset),
-                                    "test":Dataset.from_pandas(test_dataset)})
-        
+
+        self.dataset = DatasetDict({"train": Dataset.from_pandas(combined_dataset),
+                                    "test": Dataset.from_pandas(test_dataset)})
 
         for split in self.dataset.keys():
             self.dataset[split] = self.dataset[split].map(
@@ -164,21 +129,24 @@ class finetuning_data(LightningDataModule):
 
         self.eval_splits = [x for x in self.dataset.keys() if "validation" in x]
 
+
     def prepare_data(self):
-        datasets.load_from_disk(os.path.join(self.data_path, self.data_dir))
+        datasets.load_from_disk(os.path.join(self.data_dir, self.filename))
+
 
     def train_dataloader(self):
         return DataLoader(self.dataset["train"], batch_size=self.train_batch_size, shuffle=True)
 
+
     def test_dataloader(self):
         return DataLoader(self.dataset["test"], batch_size=self.eval_batch_size)
-    
+
+
     def predict_dataloader(self):
         return DataLoader(self.dataset["test"], batch_size=self.eval_batch_size)
-    
+
 
     def convert_to_features(self, example_batch, indices=None):
-
         # Either encode single sentence or sentence pairs
         if len(self.input_fields) > 1:
             texts_or_text_pairs = list(zip(example_batch[self.input_fields[0]], example_batch[self.input_fields[1]]))
@@ -194,21 +162,21 @@ class finetuning_data(LightningDataModule):
         features["labels"] = example_batch["label"]
 
         return features
-    
-    
+
+
 class finetuner(LightningModule):
     def __init__(
-        self,
-        model_name_or_path: str,
-        num_labels: int,
-        learning_rate: float = 2e-5,
-        adam_epsilon: float = 1e-8,
-        warmup_steps: int = 0,
-        weight_decay: float = 0.0,
-        train_batch_size: int = 4,
-        eval_batch_size: int = 4,
-        eval_splits: Optional[list] = None,
-        **kwargs,
+            self,
+            model_name_or_path: str,
+            num_labels: int,
+            learning_rate: float = 2e-5,
+            adam_epsilon: float = 1e-8,
+            warmup_steps: int = 0,
+            weight_decay: float = 0.0,
+            train_batch_size: int = 4,
+            eval_batch_size: int = 4,
+            eval_splits: Optional[list] = None,
+            **kwargs,
     ):
         super().__init__()
 
@@ -238,7 +206,7 @@ class finetuner(LightningModule):
 
         labels = batch["labels"]
         return {"loss": val_loss, "preds": preds, "labels": labels}
-    
+
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         val_loss, logits = self(**batch)[:2]
         if self.hparams.num_labels > 1:
@@ -247,12 +215,11 @@ class finetuner(LightningModule):
             preds = logits.squeeze()
         labels = batch["labels"]
         return [logits, preds, labels]
-    
 
     def test_epoch_end(self, outputs):
         preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
         labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
-#         soft_preds = torch.cat([x["soft_preds"] for x in outputs]).detach().cpu().numpy()
+        #         soft_preds = torch.cat([x["soft_preds"] for x in outputs]).detach().cpu().numpy()
         loss = torch.stack([x["loss"] for x in outputs]).mean()
         self.log("val_loss", loss, prog_bar=True)
         accuracy = self.acc.compute(predictions=preds, references=labels)["accuracy"]
@@ -283,7 +250,7 @@ class finetuner(LightningModule):
         scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
         return [optimizer], [scheduler]
 
-    
+
 
 
 
